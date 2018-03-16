d0270dc522d16446764a443ae0d03461f52882cf
Jacob Gillespie <jacobwgillespie@gmail.com>
2018-03-16 11:46:39 -0500

    Add TypeScript typings

---
+++ fast-case.d.ts
@@ -0,0 +1,23 @@
+export function camelize(value: string): string
+
+export function decamelize(value: string): string
+
+export function pascalize(value: string): string
+
+export function depascalize(value: string): string
+
+export function camelizeKeys(value: object): object
+
+export function decamelizeKeys(value: object): object
+
+export function pascalizeKeys(value: object): object
+
+export function depascalizeKeys(value: object): object
+
+export function camelizeKeysInPlace(value: object): object
+
+export function decamelizeKeysInPlace(value: object): object
+
+export function pascalizeKeysInPlace(value: object): object
+
+export function depascalizeKeysInPlace(value: object): object
